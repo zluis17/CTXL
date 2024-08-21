40575b485bcfa06faa7cd279ac1183fce9a1ab96
zluis <zoeluquintero@gmail.com>
2024-08-20 13:28:54 -1200

    Ipedidos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import Login from './pages/login'
-import Home  from './pages/home'
-import SingUp from './pages/singUp'
+import IPedidos from './pages/ipedidos'
+//import Login from './pages/login'
+//import Home  from './pages/home'
+//import SingUp from './pages/singUp'
 
 
 
 export default function App() {
   return (
     <div className='App'> 
-      <Login />  
-      <SingUp/>
-      <Home/>
+      <IPedidos />
   
     
     </div>
