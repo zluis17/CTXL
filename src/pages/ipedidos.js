9441d2e3c501ea905632689d9ea87cabeb8dc14c
unknown <Aprendiz@BOGDFPCGMS4130.FormacionSena.RED>
2024-08-22 11:26:10 -0500

    inventarioPedidos99%

--- src/pages/ipedidos.js
+++ src/pages/ipedidos.js
@@ -1,72 +1,86 @@
-import React, { useState } from "react";
-import logoFacebook from "../img/facebook-512.webp";
-import logoGoogle from "../img/google_logo-google_icongoogle-512.webp";
-import logoTwitter from "../img/1690643591twitter-x-logo-png.webp";
-import logoInstagram from "../img/instagram-logo-instagram-icon-transparent-free-png.webp";
-import backgroundImag from "../img/imagen1.jpg";
+import React, { useState } from 'react';
+import logoFacebook from '../img/facebook-512.webp';
+import logoGoogle from '../img/google_logo-google_icongoogle-512.webp';
+import logoTwitter from '../img/1690643591twitter-x-logo-png.webp';
+import logoInstagram from '../img/instagram-logo-instagram-icon-transparent-free-png.webp';
+import backgroundImag from '../img/imagen1.jpg';
+import '../index.css';
 
-// Estilos CSS personalizados para las animaciones
-const styles = `
-  @keyframes blinkCursor {
-    50% {
-      border-right-color: transparent;
-    }
-  }
-
-  @keyframes typeAndDelete {
-    0%, 10% {
-      width: 0;
-    }
-    45%, 55% {
-      width: 6.2em; /* ajusta el ancho según el contenido */
-    }
-    90%, 100% {
-      width: 0;
-    }
-  }
-
-  .animate-blinkCursor {
-    animation: blinkCursor 0.5s step-end infinite alternate;
-  }
-
-  .animate-typeAndDelete {
-    animation: typeAndDelete 4s steps(11) infinite;
-  }
-`;
-
-const TerminalLoader = () => {
+const MiComponente = () => {
   return (
-    <div className="border border-gray-800 bg-gray-900 text-green-400 font-mono text-sm p-6 w-48 mx-auto mt-24 shadow-lg rounded relative overflow-hidden">
-      <style>{styles}</style>
-      <div className="absolute top-0 left-0 right-0 h-6 bg-gray-800 rounded-t px-1 flex items-center">
-        <div className="text-gray-200 text-sm">Status</div>
-        <div className="ml-auto flex">
-          <div className="w-2.5 h-2.5 rounded-full bg-red-600 mx-1"></div>
-          <div className="w-2.5 h-2.5 rounded-full bg-yellow-500 mx-1"></div>
-          <div className="w-2.5 h-2.5 rounded-full bg-green-600 mx-1"></div>
+    <div className="relative">
+      {/* Fondo con cuadrícula */}
+      <div className="absolute inset-0 z-[-1] bg-[linear-gradient(to_right,_#0f0f10_1px,_transparent_1px),_linear-gradient(to_bottom,_#0f0f10_1px,_transparent_1px)] bg-[size:1rem_1rem] bg-center filter blur-sm"></div>
+
+      {/* Contenedor principal */}
+      <div id="poda" className="flex items-center justify-center relative">
+        {/* Elementos con filtros y desenfoque */}
+        <div className="absolute inset-0 z-[-1] filter blur-md max-w-[314px] max-h-[70px] rounded-lg bg-white"></div>
+        <div className="absolute inset-0 z-[-1] filter blur-sm max-w-[314px] max-h-[70px] rounded-lg bg-[#18116a]"></div>
+        <div className="absolute inset-0 z-[-1] filter blur-sm max-w-[314px] max-h-[70px] rounded-lg bg-[#1c191c]"></div>
+        <div className="absolute inset-0 z-[-1] filter blur-sm max-w-[314px] max-h-[70px] rounded-lg bg-[#000] opacity-40"></div>
+
+        {/* Contenedor del input */}
+        <div id="main" className="relative">
+          <input
+            placeholder="Buscar..."
+            type="text"
+            name="text"
+            className="bg-[#010201] border-bg-[#8922C0] w-[301px] h-[56px] rounded-lg text-white pl-[59px] pr-4 text-lg"
+          />
+          <div id="input-mask" className="absolute w-[100px] h-[20px] bg-gradient-to-r from-transparent to-purple-600 top-[18px] left-[70px] pointer-events-none"></div>
+          <div id="pink-mask" className="absolute w-[30px] h-[20px] bg-[#8922C0] top-[10px] left-[5px] filter blur-[20px] opacity-80 transition-all duration-200  to-purple-600"></div>
+          <div id="filter-icon" className="absolute top-[7px] right-[7px] w-[40px] h-[42px] rounded-lg overflow-hidden flex items-center justify-center bg-gradient-to-b from-[#8922C0] to-[#8922C0]">
+            <svg className="w-8 h-8 text-white" viewBox="0 0 24 24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none">
+              <path d="M8.16 6.65H15.83C16.47 6.65 16.99 7.17 16.99 7.81V9.09C16.99 9.56 16.7 10.14 16.41 10.43L13.91 12.64C13.56 12.93 13.33 13.51 13.33 13.98V16.48C13.33 16.83 13.1 17.29 12.81 17.47L12 17.98C11.24 18.45 10.2 17.92 10.2 16.99V13.91C10.2 13.5 9.97 12.98 9.73 12.69L7.52 10.36C7.23 10.08 7 9.55 7 9.2V7.87C7 7.17 7.52 6.65 8.16 6.65Z" stroke="#d6d6e6"></path>
+            </svg>
+          </div>
+          <div id="search-icon" className="absolute top-4 left-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <circle cx="11" cy="11" r="8" stroke="url(#search)"></circle>
+              <line x1="22" y1="22" x2="16.65" y2="16.65" stroke="url(#searchl)"></line>
+              <defs>
+                <linearGradient id="search" gradientTransform="rotate(50)">
+                  <stop offset="0%" stopColor="#f8e7f8"></stop>
+                  <stop offset="50%" stopColor="#b6a9b7"></stop>
+                </linearGradient>
+                <linearGradient id="searchl">
+                  <stop offset="0%" stopColor="#b6a9b7"></stop>
+                  <stop offset="50%" stopColor="#837484"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
         </div>
-      </div>
-      <div className="mt-6 text-xs whitespace-nowrap overflow-hidden border-r-2 border-green-400 animate-typeAndDelete animate-blinkCursor">
-        Loading...
       </div>
     </div>
   );
 };
 
+
 function IPedidos() {
   const [data, setData] = useState([
-    { id: 1, producto: "Product A", cantidad: 10 },
-    { id: 2, producto: "Product B", cantidad: 20 },
-    { id: 3, producto: "Product C", cantidad: 15 },
+    { id: 1, producto: "Producto A", cantidad: 10 },
+    { id: 2, producto: "Producto B", cantidad: 20 },
+    { id: 3, producto: "Producto C", cantidad: 15 },
   ]);
+
+  const [isInventoryOpen, setIsInventoryOpen] = useState(false);
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
 
   const handleEdit = (id) => {
-    // Implement edit functionality here
     alert(`Edit item with ID ${id}`);
+  };
+  const toggleInventory = () => {
+    setIsInventoryOpen(!isInventoryOpen);
+  };
+  const toggleRegister = () => {
+    setIsRegisterOpen(!isRegisterOpen);
   };
 
   return (
@@ -88,19 +102,80 @@ function IPedidos() {
             className="p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
           >
             Perfil
+          
           </a>
-          <a
-            href="#settings"
-            className="p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
-          >
-            Inventarios
-          </a>
-          <a
-            href="#logout"
-            className="p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
-          >
-            Registros
-          </a>
+          <div>
+            <button
+              onClick={toggleInventory}
+              className="w-full text-left p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300 flex justify-between items-center"
+            >
+              Inventarios
+              <svg
+                className={`w-4 h-4 transform transition-transform duration-300 ${isInventoryOpen ? 'rotate-180' : ''}`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+              </svg>
+            </button>
+            {isInventoryOpen && (
+              <div className="pl-6 mt-2">
+                <a
+                  href="#option1"
+                  className="block p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
+                >
+                  Opción 1
+                </a>
+                <a
+                  href="#option2"
+                  className="block p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
+                >
+                  Opción 2
+                </a>
+                <a
+                  href="#option3"
+                  className="block p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
+                >
+                  Opción 3
+                </a>
+              </div>
+            )}
+          </div>
+          <div>
+            <button
+              onClick={toggleRegister}
+              className="w-full text-left p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300 flex justify-between items-center"
+            >
+              Registro
+              <svg
+                className={`w-4 h-4 transform transition-transform duration-300 ${isRegisterOpen ? 'rotate-180' : ''}`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+              </svg>
+            </button>
+            {isRegisterOpen && (
+              <div className="pl-6 mt-2">
+                <a
+                  href="#registerOption1"
+                  className="block p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
+                >
+                  Opción de Registro 1
+                </a>
+                <a
+                  href="#registerOption2"
+                  className="block p-3 rounded-lg hover:bg-purple-600 hover:text-white transition duration-300"
+                >
+                  Opción de Registro 2
+                </a>
+              </div>
+            )}
+          </div>
         </nav>
         {/* Social Media Icons */}
         <div className="flex justify-center p-6 space-x-4 mt-auto">
@@ -140,32 +215,39 @@ function IPedidos() {
       </aside>
 
       {/* Main Content */}
-      <main className="flex-1 flex flex-col p-10 bg-gray-900 text-white bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImag})` }}>
-        <div className="flex">
-          {/* TerminalLoader */}
-          
+      <main
+        className="flex-1 flex flex-col p-10 bg-gray-900 text-white bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${backgroundImag})` }}
+      >
+
 
           {/* Content */}
-          <div className="w-3/4">
-            <h1 className="text-4xl font-bold mb-8">Inventario Pedidos</h1>
-            <div className="w-1/4 align-right ">
-            <TerminalLoader />
+          <div className=" w-3/4 margin-top-20px">
+            <div className="w-1/4 flex">
+              <h1 className="white-space:nowrap text-4xl font-bold">Inventario Pedidos</h1>
+            <div className="pl-[120vh]">
+              <MiComponente />
             </div>
+            </div> 
+          </div>
+
+          <div className="">
             <div>
-              <p className="text-3xl text-left relative top-16">Listado:</p>
+              <div className="text-3xl text-left relative top-16 ">Listado:</div>
             </div>
             <div>
               <a
                 href="#settings"
-                className="block text-right text-3xl p-8 hover:text-purple-400 width-50px"
+                className="align-text-center block text-right text-3xl p-8 hover:text-purple-400"
               >
                 Registrar Nuevo
               </a>
             </div>
+          </div>
 
             {/* Table */}
-            <div className="overflow-x-auto mx-auto max-w-4xl">
-              <table className="min-w-full bg-black text-white border rounded-lg shadow-lg">
+            <div className=" overflow-x-auto mx-auto w-full max-w-7xl">
+              <table className=" w-full bg-black text-white border rounded-lg shadow-lg">
                 <thead>
                   <tr className="bg-purple-700 border-b border-bg-black text-2xl">
                     <th className="py-1 px-2">ID</th>
@@ -176,7 +258,10 @@ function IPedidos() {
                 </thead>
                 <tbody>
                   {data.map((item) => (
-                    <tr key={item.id} className="border-b border-purple-600 text-xl">
+                    <tr
+                      key={item.id}
+                      className="border-b border-purple-600 text-2xl"
+                    >
                       <td className="py-1 px-2">{item.id}</td>
                       <td className="py-1 px-2">{item.producto}</td>
                       <td className="py-1 px-2">{item.cantidad}</td>
@@ -185,13 +270,13 @@ function IPedidos() {
                           onClick={() => handleEdit(item.id)}
                           className="text-blue-400 hover:underline mr-2 text-xl"
                         >
-                          Edit
+                          Editar
                         </button>
                         <button
                           onClick={() => handleDelete(item.id)}
                           className="text-red-400 hover:underline text-xl"
                         >
-                          Delete
+                          Eliminar
                         </button>
                       </td>
                     </tr>
@@ -199,8 +284,6 @@ function IPedidos() {
                 </tbody>
               </table>
             </div>
-          </div>
-        </div>
       </main>
     </div>
   );
